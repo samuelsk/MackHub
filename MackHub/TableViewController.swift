0236517da6738217c3ab6d8f88980dce763c2e0d
rafaelferoli <rafaelferoli@gmail.com>
2015-04-27 14:41:35 -0300

    Storyboard

---
+++ MackHub/TableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TableViewController.swift
+//  MackHub
+//
+//  Created by Rafael Fernandes de Oliveira Carvalho on 4/27/15.
+//  Copyright (c) 2015 Rafael Fernandes de Oliveira Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
