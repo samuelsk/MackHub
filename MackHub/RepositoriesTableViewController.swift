c4f75e24bc8ca01cfc6ef85c3c7b9bb12c6ed685
rafaelferoli <rafaelferoli@gmail.com>
2015-04-28 13:58:14 -0300

    Novo storyboard

---
+++ MackHub/RepositoriesTableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  RepositoriesTableViewController.swift
+//  MackHub
+//
+//  Created by Rafael Fernandes de Oliveira Carvalho on 4/27/15.
+//  Copyright (c) 2015 Rafael Fernandes de Oliveira Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class RepositoriesTableViewController: UITableViewController, UISearchBarDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBOutlet weak var repositorySearchBar: UISearchBar! {
+        didSet {
+            repositorySearchBar.delegate = self
+        }
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return 0
+    }
+    
+    // MARK: SearchBar delegate
+    
+//    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+//        let cancelButton = UIBarButtonItem(title: "Cancelar", style: UI, target: self, action: "searchBarDidCancel")
+//    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
