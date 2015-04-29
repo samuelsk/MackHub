ce62d37d29e6ba65d853d72c7f69d2420f31dde8
Samuel Shin Kim <Samuel@Samuels-Macbook-Pro.local>
2015-04-29 17:35:30 -0300

    NSManagedObject do PullRequest criado

---
+++ MackHub/PullRequest.swift
@@ -0,0 +1,16 @@
+//
+//  PullRequest.swift
+//  MackHub
+//
+//  Created by Samuel Shin Kim on 27/04/15.
+//  Copyright (c) 2015 Rafael Fernandes de Oliveira Carvalho. All rights reserved.
+//
+
+import CoreData
+
+class PullRequest: NSManagedObject {
+    
+    @NSManaged var login: String
+    @NSManaged var labels: NSSet
+    
+}
